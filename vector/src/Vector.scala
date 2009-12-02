dcf4c3995f27d26ada77bf8be563e89818c660f9
Shuji Yamamoto <yamashuu@k3.dion.ne.jp>
2009-12-02 21:12:40 +0900

    vector lib

--- vector/src/Vector.scala
+++ vector/src/Vector.scala
@@ -20,11 +20,19 @@ trait Vector[T]
 			def apply(i:Int) = op( left(i) , right(i) )
 		}
 	}
+	//
+	def metric(v:Vector[T],binOp:(T,T)=>T,foldOp:Vector[T]=>T) : T = {
+		foldOp( operate(v,binOp) )
+	}
+	//
 }
 
 object Vector
 {
 	def apply[T](xs:T*)     : Vector[T] = s2v(xs)
+	def apply[T,S<%T](v:Vector[S]) : Vector[T] = new Vector[T]{
+		def apply(i:Int) = v(i).asInstanceOf[T]
+	}
 	
 	implicit def f2v[T]( v:(Int)=>T ) : Vector[T] = new Vector[T]{
 		def apply(i:Int) = v(i)
@@ -33,6 +41,9 @@ object Vector
 	implicit def s2v[T]( v:Seq[T]) : Vector[T] = new Vector[T]{
 		def apply(i:Int) = v(i)
 	}
+	
+	implicit def v2t2[T](v:Vector[T]) : (T,T)   = (v.x , v.y)
+	implicit def v2t3[T](v:Vector[T]) : (T,T,T) = (v.x , v.y , v.z)
 	
 	implicit def t2v[T](v:(T,T)) : Vector[T] = new Vector[T]{
 		def apply(i:Int):T = i match {
