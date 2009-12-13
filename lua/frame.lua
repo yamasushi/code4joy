495860fd30cf99888a7727f2e8ef77337c650412
Shuji Yamamoto <yamashuu@k3.dion.ne.jp>
2009-12-14 08:29:00 +0900

    frame lua

---
+++ lua/frame.lua
@@ -0,0 +1,28 @@
+require "vector"
+
+Frame = {}
+Frame_mt = {__index = Frame}
+function Frame:new(param_min,param_max)
+	local o = {}
+	o.min = param_min
+	o.max = param_max
+	return setmetatable(o,Frame_mt)
+end
+Frame_mt.__tostring = function(a) return string.format("(min=%s,max=%s)", tostring(a.min) , tostring(a.max) ) end
+
+function Frame:is_inside(pt)
+	print(pt)
+	return (	(pt.x >= self.min.x) and (pt.x <= self.max.x) and
+				(pt.y >= self.min.y) and (pt.y <= self.max.y) )
+end
+
+function Frame:inflate( p )
+	--print(p)
+	local min_x = (p.x < self.min.x) and p.x or self.min.x
+	local min_y = (p.y < self.min.y) and p.y or self.min.y
+	local max_x = (p.x > self.max.x) and p.x or self.max.x
+	local max_y = (p.y > self.max.y) and p.y or self.max.y
+	return Frame:new(
+			Vector:new(min_x,min_y) ,
+			Vector:new(max_x,max_y) )
+end
