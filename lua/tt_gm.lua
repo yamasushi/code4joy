944bb2757b7337113c7cfdd3bfae5eaf2c7479cc
Shuji Yamamoto <yamashuu@k3.dion.ne.jp>
2009-12-15 08:48:15 +0900

    parameter / lua

--- lua/tt_gm.lua
+++ lua/tt_gm.lua
@@ -7,12 +7,12 @@ require "ds_gumowski_mira"
 
 img_width  = 800
 img_height = 800
-mu      =-0.485
-nu      = 1.0
-param_a = 0.008
-param_b = 0.05
+mu,nu      = 0 , 1 -- -0.485 , 1.0
+param_a,param_b = 0.008, 0.05
+oval_r = 0.1 -- 5.0
 
 max_iter= 2000
+num_traj= 100
 
 header = "A"
 pi  = function(t) return math.abs(t) end
@@ -22,12 +22,15 @@ meta_phi = function(pi,psi)
 	return function(t) return pi(t)/(1+psi(t)) end
 end
 
-gm = DSGumowskiMira:new(header,param_a,param_b,mu,nu,pi,psi,meta_phi)
-print(gm)
-chaos_system = ChaosSystem:new(gm,max_iter)
-print(chaos_system)
+params = {}
+table.insert(params,DSGumowskiMira:new("A",0.008,0.05,-0.485,1.0 , pi , psi , meta_phi))
+table.insert(params,DSGumowskiMira:new("A",0.008,0.05, 0    ,1.0 , pi , psi , meta_phi))
 
-cr = ChaosRenderer:new(chaos_system,100,5.0)
-print(cr)
-cr:render(img_width,img_height)
-
+for i,gm in ipairs(params) do
+	print(i,gm)
+	chaos_system = ChaosSystem:new(gm,max_iter)
+	--print(chaos_system)
+	cr = ChaosRenderer:new(chaos_system,num_traj,oval_r)
+	--print(cr)
+	cr:render(img_width,img_height)
+end
