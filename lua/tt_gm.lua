a34b61e819fb32699056a0b01605647006ad35f8
Shuji Yamamoto <yamashuu@k3.dion.ne.jp>
2009-12-15 11:36:37 +0900

    BUG:global meta_phi

--- lua/tt_gm.lua
+++ lua/tt_gm.lua
@@ -5,28 +5,28 @@ require "chaos_system"
 
 require "ds_gumowski_mira"
 
-img_width  = 800
-img_height = 800
-oval_r     = 0.1
+local img_width  = 800
+local img_height = 800
+local oval_r     = 0.1
 
-max_iter= 2000
-num_traj= 100
+local max_iter= 2000
+local num_traj= 100
 
-header = {"_","A"}
+local header = {"_","A"}
 
-pi={}
+local pi={}
 pi["A"] = math.abs
 pi["_"] = function(t) return t*t end
 
-psi={}
+local psi={}
 psi["A"] = math.abs
 psi["_"] = function(t) return t*t end
 
-meta_phi = function(param_pi,param_psi)
+local meta_phi = function(param_pi,param_psi)
 	return function(t) return param_pi(t)/(1+param_psi(t)) end
 end
 
-params = {}
+local params = {}
 for i,h in ipairs(header) do
 	--print(i,h,pi[h],psi[h])
 	table.insert(params,DSGumowskiMira:new(h,0.008,0.05,-0.485,1.0 , pi[h] , psi[h] , meta_phi))
