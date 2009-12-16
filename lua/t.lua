9c171c00a3f38db001ffa0ffa1a450e08b5ede61
Shuji Yamamoto <yamashuu@k3.dion.ne.jp>
2009-12-16 14:35:54 +0900

    sampling walk test

--- lua/t.lua
+++ lua/t.lua
@@ -1,4 +1,20 @@
 require "Lattice"
+require "gd_bitmap"
 
---print (Lattice.scaler)
-print (Lattice:nth_scaler(11))
+local l  = Lattice:new(3 ,2.0)
+print(l)
+local sp = l:pos ( {0,0} )
+local ep = l:pos ( {0,0} )
+
+make_bitmap_png("a.png",1000,1000,
+	function (im)
+		local black = im:colorAllocate(  0,   0,   0)
+		local white = im:colorAllocate(255, 255, 255)
+		Lattice:scan(4,{250,150},
+			function(ip)
+				print( ip )
+				sp = ep
+				ep = l:pos(ip)
+				im:line(sp[1],sp[2],ep[1],ep[2],black)
+			end )
+	end )
