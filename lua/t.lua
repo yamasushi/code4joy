6c10e238e28e5b3fcc66e243920a7eee18e109bd
Shuji Yamamoto <yamashuu@k3.dion.ne.jp>
2009-12-16 13:53:47 +0900

    lattice scaler

--- lua/t.lua
+++ lua/t.lua
@@ -1,9 +1,4 @@
-require "Matrix"
+require "Lattice"
 
-print (Matrix.zero)
-print (Matrix.unit)
-local a = Matrix:new({-2,3},{1,2})
-print(-a)
-print(a(Vector:new(0,1)))
-print(a(Vector:new(1,0)))
-
+--print (Lattice.scaler)
+print (Lattice:nth_scaler(11))
