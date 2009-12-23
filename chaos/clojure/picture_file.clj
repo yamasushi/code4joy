8ff1ec575c26173ea0eaea4f22cc7af98c3e0f50
Shuji Yamamoto <yamashuu@k3.dion.ne.jp>
2009-12-23 19:16:29 +0900

    picture file output /clojure

--- chaos/clojure/picture_file.clj
+++ chaos/clojure/picture_file.clj
@@ -2,14 +2,18 @@
 (import '(java.awt Color))
 (import '(java.io File))
 (import '(javax.imageio ImageIO))
-(let  [ bi (BufferedImage. 1000 2000 BufferedImage/TYPE_BYTE_GRAY)
-        g  (.createGraphics bi) 
-        f  (File. "a.png") ] 
-  (.setPaint g Color/WHITE)
-  (.fillRect g 0,0,1000,2000)
-  (.setColor g Color/BLACK)
-  (.drawLine g 0,0,100,100)
-  (.drawLine g 30,20,100,100)
-  (ImageIO/write bi "png",f)
+
+(defn create-picture-png [width height filename op]
+  (let  [ bi (BufferedImage. width height BufferedImage/TYPE_BYTE_GRAY)
+          g  (.createGraphics bi) 
+          f  (File. filename) ] 
+    (.setPaint g Color/WHITE)
+    (.fillRect g 0,0,width,height)
+    (.setColor g Color/BLACK)
+    ;(println g)
+    (op g)
+    (ImageIO/write bi "png",f)
+  )
 )
 
+(create-picture-png 500 600 "a.png" (fn [g] (.drawLine g 0 0 100 100) ) )
