134249f47214749e23119bec6bedc56fcecd3d18
Shuji Yamamoto <yamashuu@k3.dion.ne.jp>
2009-12-24 19:18:44 +0900

    simplified gumowski-mira test/clojure

--- chaos/clojure/t.clj
+++ chaos/clojure/t.clj
@@ -1,9 +1,16 @@
 (load-file "picture_file.clj")
 
+(def  a    -0.9)
+(def  b    0.96)
+(def  c   -4.0 )
+(def  d    0.9 )
+(defn pi  [t] (* t t))
+(defn psi [t] (* t t))
+
 (defn calc-next [pt]
   (let [ [x y] pt]
     ;(println x y)
-    [ (+ 1.0 (- y) (Math/abs x) ) , x]
+    [ (+ (* a x) (* b y) (/ (+ (* c (pi x) ) d ) (+ 1 (psi x) ) )) , (- x)]
     ))
 
 (defn transform [pt]
@@ -11,12 +18,13 @@
   [(* x 100),(* y 100)]
   ))
 
-(create-picture-png 500 600 "a.png" (fn [g]
-  (loop [pt [0.01,0.] , n 1000]
-    (println (transform pt))
+(create-picture-png 1000 600 "a.png" (fn [g]
+  (loop [pt [-1.0 , 0.0] , n 100000]
+    ;(println (transform pt))
     (let [[x y] (transform pt)] (.drawLine g x y x y) )
     (if (zero? n)
       pt
       (recur (calc-next pt) (dec n) )))
       
       ))
+
